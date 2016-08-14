35ec97d82af0d04456d879020c8923448f713f3a
lauricdd <lauri_cdd@hotmail.com>
2016-08-14 13:56:53 -0500

    Guardar en la base de datos las actualizaciones de registros de aplicaciones

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,22 @@
+@extends('base')
+
+@section('title', 'Apps - 404')
+
+@section('content')
+    <div class="container">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="page-header">
+                <h1>404 <small>/ NO ENCONTRADO</small></h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <br>
+                <h4 class="text-center">La página que estabas buscando  no existe.</h4>
+                <br>
+                <h4 class="text-center"><a href="/apps">Regresar</a></h4>
+            </div>
+        </div>
+    </div>
+@stop
